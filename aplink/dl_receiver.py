659927728b576dcb3cd049dec8fbc921c358d44e
Fabrizio Scimia <fabrizio.scimia@gmail.com>
2016-03-14 22:31:34 +0000

    General code refactoring: removed "magic numbers" decoding logic pushed into message object bug fix: LastFragment and QCI not correctly decoded

--- aplink/dl_receiver.py
+++ aplink/dl_receiver.py
@@ -13,11 +13,13 @@ Revision History:
 """
 
 import util.airpy_logger as logger
+from aplink.messages.ap_enable_message import EnableMessage
+from aplink.messages.ap_disable_message import DisableMessage
 
 
 class DLReceiver:
 
-    def __init__(self,apl_manager, streamer, h_builder):
+    def __init__(self, apl_manager, streamer, h_builder):
         self.byte_streamer = streamer
         self.header_builder = h_builder
         self.aplink_manager = apl_manager
@@ -25,12 +27,16 @@ class DLReceiver:
         self.startByteFound = False
         self.byteIndex = 0
         self.messageId = 0
-        self.CQI = 3
+        self.QCI = 3
         self.lastFragment = 0
         self.messageTypeId = 0
         self.payloadLength = 0
         self.EOF = None
         self.payload = None
+
+        # reporting
+        self.valid_msg_count = 0
+        self.discarded_msg_count = 0
 
     def read_byte(self):
         # Read a new byte from the serial connection
@@ -50,20 +56,20 @@ class DLReceiver:
 
     def parse_header(self):
 
-        if self.byteIndex == 1:
+        if self.byteIndex == self.header_builder.MESSAGE_ID_BYTE_1:
             self.messageId = self.tmpByte[0] << 8
 
-        elif self.byteIndex == 2:
+        elif self.byteIndex == self.header_builder.MESSAGE_ID_BYTE_2:
             self.messageId += self.tmpByte[0]
 
-        elif self.byteIndex == 3:
-            self.CQI = self.tmpByte[0] & 0xF8 >> 3
+        elif self.byteIndex == self.header_builder.QCI_AND_LAST_FRAGMENT:
+            self.QCI = (self.tmpByte[0] & 0xF8) >> 3
             self.lastFragment = self.tmpByte[0] & 0x07
 
-        elif self.byteIndex == 4:
+        elif self.byteIndex == self.header_builder.MESSAGE_TYPE_ID:
             self.messageTypeId = self.tmpByte[0]
 
-        elif self.byteIndex == 11:
+        elif self.byteIndex == self.header_builder.PAYLOAD_LENGTH:
             self.payloadLength = self.tmpByte[0]
 
         self.byteIndex += 1
@@ -72,17 +78,20 @@ class DLReceiver:
         if self.byteIndex == self.header_builder.HEADER_LEN + self.payloadLength:
 
             if self.tmpByte[0] == self.EOF:
-                logger.info("incoming message ready for decoding")
                 self.decode_payload(self.messageTypeId, self.payload)
+                self.valid_msg_count += 1
+                # debug
+                # if self.valid_msg_count == 1:
+                #    logger.debug("messageID:{}, QCI:{}, LastFragment:{}".format(self.messageId, self.QCI, self.lastFragment))
             else:
-                logger.info("incoming message discarded")
+                self.discarded_msg_count += 1
 
             self.byteIndex = -1
             self.startByteFound = False
 
         elif self.byteIndex == self.header_builder.HEADER_LEN:  # 1st byte of the payload is replicated at the EOF
             self.EOF = self.tmpByte[0]
-            self.payload = bytearray(self.payloadLength);
+            self.payload = bytearray(self.payloadLength)
             self.payload[0] = self.tmpByte[0]
 
         else:
@@ -92,7 +101,7 @@ class DLReceiver:
 
     def decode_payload(self, message_type_id, payload):
 
-        if message_type_id == 40:
-            self.aplink_manager.set_message_status(payload[0], 1)
-        elif message_type_id == 50:
-            self.aplink_manager.set_message_status(payload[0], 0)
+        if message_type_id == EnableMessage.MESSAGE_TYPE_ID:
+            self.aplink_manager.set_message_status(EnableMessage.decode_payload(payload), 1)
+        elif message_type_id == DisableMessage.MESSAGE_TYPE_ID:
+            self.aplink_manager.set_message_status(EnableMessage.decode_payload(payload), 0)
--- aplink/header_builder.py
+++ aplink/header_builder.py
@@ -13,7 +13,6 @@ Revision History:
 """
 
 import array
-import binascii
 
 
 class HeaderBuilder:
@@ -29,11 +28,24 @@ class HeaderBuilder:
         self.LAST_FRAGMENT_TRUE = 7
         self.LAST_FRAGMENT_FALSE = 0
 
+        # header bytes index
+        self.START_BYTE = 0
+        self.MESSAGE_ID_BYTE_1 = 1
+        self.MESSAGE_ID_BYTE_2 = 2
+        self.QCI_AND_LAST_FRAGMENT = 3
+        self.MESSAGE_TYPE_ID = 4
+        self.LINK_ID = 5
+        self.FRAGMENT_NUM_BYTE_1 = 6
+        self.FRAGMENT_NUM_BYTE_2 = 7
+        self.SEQUENCE_NUM_BYTE_1 = 8
+        self.SEQUENCE_NUM_BYTE_2 = 9
+        self.FAIL_SAFE_AND_FLIGHT_MODE = 10
+        self.PAYLOAD_LENGTH = 11
+
         # class variables
         self.sequenceNumber = 0
         self.tempMessageID = 10858  # TODO to be retrieved randomly
         self.header = array.array('B', (0,) * self.HEADER_LEN)  # RC Channels
-        # self.header = bytearray(13)  # RC Channels
         self.tmp_message = None
 
     def get_header(self, msg):
@@ -46,16 +58,15 @@ class HeaderBuilder:
         return self.header
 
     def build_header(self):
-        self.header[0] = 15  # \x0F
-        self.header[1] = (self.tempMessageID & self.SECOND_BYTE_BITMASK) >> 8
-        self.header[2] = self.tempMessageID & self.FIRST_BYTE_BITMASK
-        self.header[3] = (self.tmp_message.QCI & self.FIRST_BYTE_BITMASK << 3) + (self.LAST_FRAGMENT_TRUE & self.FIRST_BYTE_BITMASK)
-        self.header[4] = self.tmp_message.MESSAGE_TYPE_ID & self.FIRST_BYTE_BITMASK
-        self.header[5] = 0  # Link ID TODO TB implemented
-        self.header[6] = 0  # Fragment Number Byte 1 TODO TB implemented
-        self.header[7] = 0  # Fragment Number Byte 2 TODO TB implemented
-        self.header[8] = 0  # Sequence Number Byte 1 TODO TB implemented
-        self.header[9] = 0  # Sequence Number Byte 2 TODO TB implemented
-        self.header[10] = (self.tmp_message.FAIL_SAFE & self.FIRST_BYTE_BITMASK) << 4  # Failsafe & Flight Mode TODO Flight Mode TB implemented
-        self.header[11] = self.tmp_message.PAYLOAD_LENGTH & self.FIRST_BYTE_BITMASK
-        # self.header[12] = self.tmp_message.EOF & self.FIRST_BYTE_BITMASK  # EOF = 1st Payload Byte
+        self.header[self.START_BYTE] = 15  # \x0F
+        self.header[self.MESSAGE_ID_BYTE_1] = (self.tempMessageID & self.SECOND_BYTE_BITMASK) >> 8
+        self.header[self.MESSAGE_ID_BYTE_2] = self.tempMessageID & self.FIRST_BYTE_BITMASK
+        self.header[self.QCI_AND_LAST_FRAGMENT] = (self.tmp_message.QCI & self.FIRST_BYTE_BITMASK << 3) + (self.LAST_FRAGMENT_TRUE & self.FIRST_BYTE_BITMASK)
+        self.header[self.MESSAGE_TYPE_ID] = self.tmp_message.MESSAGE_TYPE_ID & self.FIRST_BYTE_BITMASK
+        self.header[self.LINK_ID] = 0  # Link ID TODO TB implemented
+        self.header[self.FRAGMENT_NUM_BYTE_1] = 0  # Fragment Number Byte 1 TODO TB implemented
+        self.header[self.FRAGMENT_NUM_BYTE_2] = 0  # Fragment Number Byte 2 TODO TB implemented
+        self.header[self.SEQUENCE_NUM_BYTE_1] = 0  # Sequence Number Byte 1 TODO TB implemented
+        self.header[self.SEQUENCE_NUM_BYTE_2] = 0  # Sequence Number Byte 2 TODO TB implemented
+        self.header[self.FAIL_SAFE_AND_FLIGHT_MODE] = (self.tmp_message.FAIL_SAFE & self.FIRST_BYTE_BITMASK) << 4  # Failsafe & Flight Mode TODO Flight Mode TB implemented
+        self.header[self.PAYLOAD_LENGTH] = self.tmp_message.PAYLOAD_LENGTH & self.FIRST_BYTE_BITMASK
